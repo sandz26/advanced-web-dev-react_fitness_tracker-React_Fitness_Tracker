f679de771c4cf6c9c225bdcdfb9bad044a31ddf9
sandz-jalusi <sandile@jalusitech.co.za>
2026-09-04 00:09:08 +0200

    edit app entrypoint

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+      
+      </section>
+    </>
+  )
+}
+
+export default App
